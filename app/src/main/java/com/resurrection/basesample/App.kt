9b0b9b2d4b2855e200213359521349ebf36bdc30
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-06 15:57:41 +0300

    Dependencies was added.

---
+++ app/src/main/java/com/resurrection/basesample/App.kt
@@ -0,0 +1,8 @@
+package com.resurrection.basesample
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class App : MultiDexApplication()
