93e599bf20715bb424de06a4733f3fe66855d9a4
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-06 17:32:04 +0300

    AppButton was added.

--- app/src/main/java/com/resurrection/basesample/base/widget/AppButton.kt
+++ app/src/main/java/com/resurrection/basesample/base/widget/AppButton.kt
@@ -1,11 +1,14 @@
 package com.resurrection.basesample.base.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import androidx.core.view.marginTop
+import android.view.MotionEvent
 import com.google.android.material.button.MaterialButton
 import com.resurrection.basesample.R
+import com.resurrection.basesample.base.util.startCustomAnimation
 
+@SuppressLint("ClickableViewAccessibility")
 class AppButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -14,7 +17,14 @@ class AppButton @JvmOverloads constructor(
 
     init {
         setBackgroundResource(R.drawable.rounded_rectangle_bg)
+
+        this.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> startCustomAnimation(R.anim.button_click)
+            }
+            false
+        }
     }
-
-
 }
+
+
