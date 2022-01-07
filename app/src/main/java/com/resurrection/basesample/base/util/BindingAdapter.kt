0877d8f745029926aa1d83355f5b01c75cdd6c17
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-07 10:34:19 +0300

    AppImageView was added.

--- app/src/main/java/com/resurrection/basesample/base/util/BindingAdapter.kt
+++ app/src/main/java/com/resurrection/basesample/base/util/BindingAdapter.kt
@@ -7,25 +7,25 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.resurrection.basesample.base.general.tryCatch
 
 @BindingAdapter("loadImageFromUrl")
-fun ImageView.loadImage(imageUrl: String?) {
-
-    val circularProgressDrawable = CircularProgressDrawable(this.context)
-    circularProgressDrawable.strokeWidth = 10f
-    circularProgressDrawable.centerRadius = 30f
-    circularProgressDrawable.setArrowDimensions(30f, 30f)
-    circularProgressDrawable.start()
-
-    imageUrl?.let {
-        var requestOptions = RequestOptions()
-        requestOptions = requestOptions.transforms(CenterCrop(), RoundedCorners(25))
-        Glide.with(this)
-            .load(imageUrl)/*.override(500,750)*/
-            .placeholder(circularProgressDrawable)
-            //.error(R.drawable.image_not_found)  // any image in case of error
-            .apply(requestOptions)
-            .into(this)
+fun ImageView.loadImage(imageUrl: Any?) {
+    tryCatch {
+        imageUrl?.let {
+            val circularProgressDrawable = CircularProgressDrawable(this.context)
+            circularProgressDrawable.strokeWidth = 10f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.setArrowDimensions(30f, 30f)
+            circularProgressDrawable.start()
+            val requestOptions = RequestOptions().transforms(CenterCrop(), RoundedCorners(52))
+            Glide.with(this)
+                .load(imageUrl)/*.override(500,750)*/
+                .placeholder(circularProgressDrawable)
+                //.error(R.drawable.image_not_found)  // any image in case of error
+                .apply(requestOptions)
+                .into(this)
+        }
     }
 }
 
