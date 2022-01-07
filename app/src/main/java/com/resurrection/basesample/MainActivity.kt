91d3d633c9848a343436892ecf88d90b975e6efa
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-07 09:59:57 +0300

    AppEditText was added.

--- app/src/main/java/com/resurrection/basesample/MainActivity.kt
+++ app/src/main/java/com/resurrection/basesample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.resurrection.basesample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,11 +9,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         findViewById<Button>(R.id.button).setOnClickListener {
             toast("Hello World")
         }
+
+
     }
 }
