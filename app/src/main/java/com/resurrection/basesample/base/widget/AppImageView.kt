5cb4db35e02b2e43beb2ff595bcd9652f4fd9e28
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-11 22:58:46 +0300

    LibraryModule was removed

--- app/src/main/java/com/resurrection/basesample/base/widget/AppImageView.kt
+++ app/src/main/java/com/resurrection/basesample/base/widget/AppImageView.kt
@@ -2,19 +2,9 @@ package com.resurrection.basesample.base.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import coil.load
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.imageview.ShapeableImageView
 import com.resurrection.basesample.R
-import com.resurrection.basesample.base.general.tryCatch
 import com.resurrection.basesample.base.util.loadImage
-import kotlin.math.round
 
 class AppImageView @JvmOverloads constructor(
     context: Context,
@@ -24,7 +14,6 @@ class AppImageView @JvmOverloads constructor(
 
     init {
         this.loadImage(R.drawable.ic_launcher_background)
-
     }
 
 }
