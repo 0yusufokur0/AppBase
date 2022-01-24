a955cee15abfff74d19cc434745906e2076ccbd2
Yusuf Okur <yusuf.okur@veripark.com>
2022-01-24 12:32:40 +0300

    Cryptography was added.

--- app/src/main/java/com/resurrection/appbase/ui/main/photos/PhotosFragment.kt
+++ app/src/main/java/com/resurrection/appbase/ui/main/photos/PhotosFragment.kt
@@ -9,15 +9,25 @@ import com.resurrection.appbase.databinding.FragmentPhotosBinding
 import com.resurrection.base.core.BaseAdapter
 import com.resurrection.base.core.BaseFragment
 import com.resurrection.appbase.data.model.photos.PhotoModelItem
+import com.resurrection.base.data.Cryptography
 
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class PhotosFragment : BaseFragment<FragmentPhotosBinding, PhotosViewModel>
     (R.layout.fragment_photos, PhotosViewModel::class.java) {
 
     override fun init(savedInstanceState: Bundle?) {
+
+
+        val pass = "12345678"
+        val text = "aaa"
+        val encrypted = Cryptography.encrypt(pass, text)
+        println(encrypted)
+        val decrypted = Cryptography.decrypt(pass, encrypted)
+        println(decrypted)
+
+
 
 
         var adapter = BaseAdapter(R.layout.photo_item, BR.photoItem, arrayListOf<PhotoModelItem>()){
