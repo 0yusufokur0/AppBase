8df0d8ad1827004293e692fa6a283cf2f2df35a8
Yusuf Okur <0yusufokur0@gmail.com>
2022-01-30 23:49:40 +0300

    BaseRepository was added.

---
+++ base/src/main/java/com/resurrection/base/core/BaseRepository.kt
@@ -0,0 +1,40 @@
+package com.resurrection.base.core
+
+import com.resurrection.base.util.Resource
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+
+open class BaseRepository {
+
+    @JvmName("getData1")
+    fun <T> getData(request: suspend () -> Response<T>) =  flow { emit(getResourceByNetworkRequest { request() }) }
+    fun <T> getData(request: suspend () -> T) = flow { emit(getResourceByDatabaseRequest { request() }) }
+
+    suspend fun <T> getResourceByNetworkRequest(request: suspend () -> Response<T>): Resource<T> {
+        try {
+            val response = request()
+            if (response.isSuccessful) {
+                response.body()?.apply {
+                    return Resource.Success(this)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Resource.Error(e)
+        }
+        return Resource.Loading()
+    }
+
+    suspend fun <T> getResourceByDatabaseRequest(request: suspend () -> T): Resource<T> {
+        try {
+            val result = request()
+            result?.let {
+                return Resource.Success(result)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Resource.Error(e)
+        }
+        return Resource.Loading()
+    }
+}
--- base/src/main/java/com/resurrection/base/util/Resource.kt
+++ base/src/main/java/com/resurrection/base/util/Resource.kt
@@ -17,34 +17,3 @@ sealed class Resource<out T>(val status: Status, val data: T?, val message: Thro
     class InValid<T>(exception: Throwable?) : Resource<T>(Status.INVALID, null, exception)
 }
 
-@JvmName("getData1")
-fun <T> getData(request: suspend () -> Response<T>) =  flow { emit(getResourceByNetworkRequest { request() }) }
-fun <T> getData(request: suspend () -> T) = flow { emit(getResourceByDatabaseRequest { request() }) }
-
-suspend fun <T> getResourceByNetworkRequest(request: suspend () -> Response<T>): Resource<T> {
-    try {
-        val response = request()
-        if (response.isSuccessful) {
-            response.body()?.apply {
-                return Resource.Success(this)
-            }
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        return Resource.Error(e)
-    }
-    return Resource.Loading()
-}
-
-suspend fun <T> getResourceByDatabaseRequest(request: suspend () -> T): Resource<T> {
-    try {
-        val result = request()
-        result?.let {
-            return Resource.Success(result)
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        return Resource.Error(e)
-    }
-    return Resource.Loading()
-}
