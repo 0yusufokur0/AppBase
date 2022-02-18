ee950d20de9663ea01aa49cc411d7f35acaaa714
Yusuf Okur <yusuf.okur@veripark.com>
2022-02-18 14:24:28 +0300

    Widget util was added.

---
+++ base/src/main/java/com/resurrection/base/widget/Util.kt
@@ -0,0 +1,15 @@
+package com.resurrection.base.widget
+
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+fun RecyclerView.setGridLayoutManager(spanCount:Int) =
+    GridLayoutManager(this.context, spanCount).also { this.layoutManager = it }
+
+fun RecyclerView.setVerticalLinearLayoutManager(reverseLayout:Boolean = false) =
+    LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, reverseLayout)
+
+fun RecyclerView.setHorizontalLinearLayoutManager(reverseLayout:Boolean = false) =
+    LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, reverseLayout)
