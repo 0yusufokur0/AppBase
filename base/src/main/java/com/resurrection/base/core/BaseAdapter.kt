f920efb9a1f838c6d95d1c3b8b270023fc489c9f
Yusuf Okur <0yusufokur0@gmail.com>
2022-02-21 20:29:25 +0300

    Unnecessary methods was removed

--- base/src/main/java/com/resurrection/base/core/BaseAdapter.kt
+++ base/src/main/java/com/resurrection/base/core/BaseAdapter.kt
@@ -1,5 +1,6 @@
 package com.resurrection.base.core
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
@@ -10,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.resurrection.base.component.*
 import java.util.*
 import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 open class BaseAdapter<T>(
     private val layoutResource: Int,
@@ -19,23 +19,29 @@ open class BaseAdapter<T>(
     private val onItemClick: (T) -> Unit
 ) : RecyclerView.Adapter<BaseAdapter.BaseHolder<T>>() {
 
-    @Inject lateinit var appState: AppState
-    @Inject lateinit var dataHolder: DataHolderManager
-    @Inject lateinit var sharedPreferences: SharedPreferencesManager
-    @Inject lateinit var logger: Logger
-    @Inject lateinit var loadingIndicator: AppLoadingIndicator
-    @Inject lateinit var networkManager: NetworkManager
-    @Inject lateinit var securityManager: SecurityManager
+    @Inject
+    lateinit var appState: AppState
+    @Inject
+    lateinit var dataHolder: DataHolderManager
+    @Inject
+    lateinit var sharedPreferences: SharedPreferencesManager
+    @Inject
+    lateinit var logger: Logger
+    @Inject
+    lateinit var loadingIndicator: AppLoadingIndicator
+    @Inject
+    lateinit var networkManager: NetworkManager
+    @Inject
+    lateinit var securityManager: SecurityManager
+
     lateinit var binding: ViewDataBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseHolder<T> {
-         binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), layoutResource, parent, false)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), layoutResource, parent, false)
         return BaseHolder(binding, itemId, onItemClick)
     }
 
-    override fun onBindViewHolder(holder: BaseHolder<T>, position: Int) {
-        currentList?.let { holder.bind(currentList!![position]) }
-    }
+    override fun onBindViewHolder(holder: BaseHolder<T>, position: Int) { currentList?.let { holder.bind(currentList!![position]) } }
 
     override fun getItemCount() = currentList?.let { currentList!!.size } ?: run { 0 }
 
@@ -50,43 +56,6 @@ open class BaseAdapter<T>(
         }
     }
 
-    fun <R> filterBy(constraint: CharSequence, selector: ((T) -> R?)? = null) {
-        currentList?.let {
-            val mFilter = object : Filter() {
-                override fun performFiltering(constraint: CharSequence?): FilterResults {
-                    val filteredList = ArrayList<T>()
-                    if (constraint == null || constraint.isEmpty()) {
-                        filteredList.addAll(currentList!!)
-                    } else {
-                        val filterPattern = constraint.toString().lowercase(Locale.getDefault()).trim()
-
-                        selector?.let {
-                            for (item in currentList!!)
-                                if (it.invoke(item).toString().lowercase(Locale.getDefault())
-                                        .contains(filterPattern)
-                                ) filteredList.add(
-                                    item
-                                )
-                        } ?: run {
-                            for (item in currentList!!)
-                                if (item.toString().lowercase(Locale.getDefault()).contains(filterPattern)) filteredList.add(item)
-                        }
-                    }
-                    val results = FilterResults()
-                    results.values = filteredList
-                    return results
-                }
-
-                override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
-                    currentList!!.clear()
-                    currentList!!.addAll(results?.values as ArrayList<T>)
-                    notifyDataSetChanged()
-                }
-            }
-            mFilter.filter(constraint)
-        }
-    }
-
     class BaseDiffUtil<T>(
         private val oldList: List<T>,
         private val newList: List<T>
@@ -97,55 +66,76 @@ open class BaseAdapter<T>(
         override fun areContentsTheSame(oldPosition: Int, newPosition: Int) = oldList[oldPosition] == newList[newPosition]
     }
 
-    fun addAll(list: List<T>) {
-        currentList?.let {
-            val diffUtil = BaseDiffUtil(currentList!!, list)
-            val diffResult = DiffUtil.calculateDiff(diffUtil)
-            currentList!!.clear()
-            currentList!!.addAll(list)
-            diffResult.dispatchUpdatesTo(this)
+    fun <R> filterBy(constraint: CharSequence, selector: ((T) -> R?)? = null) = currentList?.let {
+        val mFilter = object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = ArrayList<T>()
+                if (constraint == null || constraint.isEmpty()) filteredList.addAll(currentList!!)
+                else {
+                    val filterPattern = constraint.toString().lowercase(Locale.getDefault()).trim()
+                    selector?.let {
+                        for (item in currentList!!)
+                            if (it.invoke(item).toString().lowercase(Locale.getDefault()).contains(filterPattern))
+                                filteredList.add(item)
+                    } ?: run {
+                        for (item in currentList!!)
+                            if (item.toString().lowercase(Locale.getDefault()).contains(filterPattern))
+                                filteredList.add(item)
+                    }
+                }
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                currentList!!.clear()
+                currentList!!.addAll(results?.values as ArrayList<T>)
+                notifyDataSetChanged()
+            }
         }
+        mFilter.filter(constraint)
+
     }
 
-    fun add(item: T) {
-        currentList?.let {
-            currentList!!.add(item)
-            notifyItemInserted(currentList!!.size - 1)
-        }
+    fun addAll(list: List<T>) = currentList?.let {
+        val diffUtil = BaseDiffUtil(currentList!!, list)
+        val diffResult = DiffUtil.calculateDiff(diffUtil)
+        currentList!!.clear()
+        currentList!!.addAll(list)
+        diffResult.dispatchUpdatesTo(this)
     }
 
-    fun remove(item: T) {
-        currentList?.let {
-            val position = currentList!!.indexOf(item)
-            currentList!!.remove(item)
-            notifyItemRemoved(position)
-        }
+    fun add(item: T) = currentList?.let {
+        currentList!!.add(item)
+        notifyItemInserted(currentList!!.size - 1)
     }
 
-    fun update(item: T) {
-        currentList?.let {
-            val position = currentList!!.indexOf(item)
-            currentList!![position] = item
-            notifyItemChanged(position)
-        }
-
-        fun clear() {
-            currentList?.clear()
-            notifyDataSetChanged()
-        }
-
-        fun <R : Comparable<R>> sort(selector: (T) -> R?): Unit {
-            var mutable = currentList?.toMutableList()
-            mutable?.sortBy { selector(it) }
-            addAll(mutable?.toList() as ArrayList<T>)
-        }
-
-        fun getItem(position: Int) = currentList?.let { currentList!![position] }
-
-        fun getItems() = currentList?.let { currentList!!.toList() }
-
-        fun getItems(predicate: (T) -> Boolean) = currentList?.let { currentList!!.filter(predicate) }
-
+    fun remove(item: T) = currentList?.let {
+        val position = currentList!!.indexOf(item)
+        currentList!!.remove(item)
+        notifyItemRemoved(position)
     }
+
+    fun update(item: T) = currentList?.let {
+        val position = currentList!!.indexOf(item)
+        currentList!![position] = item
+        notifyItemChanged(position)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun clear() {
+        currentList?.clear()
+        notifyDataSetChanged()
+    }
+
+    fun <R : Comparable<R>> sort(selector: (T) -> R?) {
+        val mutable = currentList?.toMutableList()
+        mutable?.sortBy { selector(it) }
+        addAll(mutable?.toList() as ArrayList<T>)
+    }
+
+    fun getItem(position: Int) = currentList?.let { currentList!![position] }
 
 }
