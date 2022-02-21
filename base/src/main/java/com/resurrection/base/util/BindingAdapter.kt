7a2f284192baeb1466322fed3bd28e64c30eb72c
Yusuf Okur <0yusufokur0@gmail.com>
2022-02-21 20:29:44 +0300

    Dependencies was updated.

--- base/src/main/java/com/resurrection/base/util/BindingAdapter.kt
+++ base/src/main/java/com/resurrection/base/util/BindingAdapter.kt
@@ -1,18 +1,11 @@
 package com.resurrection.base.util
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import coil.imageLoader
-import coil.load
 import coil.loadAny
 import coil.transform.RoundedCornersTransformation
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-import com.resurrection.base.general.tryCatch
 
 @BindingAdapter("loadImage")
 fun ImageView.loadImage(image: Any?) {
