d8fa7b449f68e30cb3903c217fcc02704ead60ee
Yusuf Okur <yusuf.okur@veripark.com>
2022-02-26 18:51:57 +0300

    NetworkManager was updated.

--- base/src/main/java/com/resurrection/base/component/NetworkManager.kt
+++ base/src/main/java/com/resurrection/base/component/NetworkManager.kt
@@ -16,52 +16,50 @@ class NetworkManager @Inject constructor(@ApplicationContext val context: Contex
     private val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
     private var _isNetworkAvailable = checkNetworkAvailable()
     val isNetworkAvailable: Boolean = _isNetworkAvailable
-    private lateinit var activity:AppCompatActivity
 
-    fun init(activity: AppCompatActivity) { this.activity = activity }
-
-    fun checkNetworkAvailable(): Boolean = try {
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> true
-            }
-        } else false
-    } catch (e: Exception) {
-        false
-    }
+    fun checkNetworkAvailable(): Boolean =
+        try {
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> true
+                }
+            } else false
+        } catch (e: Exception) {
+            false
+        }
 
     fun isVpnActive() = capabilities!!.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
 
     @RequiresApi(Build.VERSION_CODES.N)
-    fun setNetworkStateListener(available:()->Unit,unAvailable:()->Unit) {
+    fun setNetworkStateListener(available: () -> Unit, unAvailable: () -> Unit) {
 
         connectivityManager.registerDefaultNetworkCallback(object :
             ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
                 _isNetworkAvailable = true
-                activity?.runOnUiThread { available() }
+                available()
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
                 _isNetworkAvailable = false
-                activity?.runOnUiThread { unAvailable() }
+                unAvailable()
             }
 
             override fun onLosing(network: Network, maxMsToLive: Int) {
                 super.onLosing(network, maxMsToLive)
                 _isNetworkAvailable = false
-                activity?.runOnUiThread { unAvailable() }
+                unAvailable()
             }
 
             override fun onUnavailable() {
                 super.onUnavailable()
                 _isNetworkAvailable = false
-                activity?.runOnUiThread { unAvailable() }
+                unAvailable()
             }
 
         })
