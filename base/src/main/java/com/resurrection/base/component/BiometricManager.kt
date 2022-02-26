35ef7c730534463fc546b72efd57d0053cab45ad
Yusuf Okur <yusuf.okur@veripark.com>
2022-02-26 17:42:57 +0300

    BiometricManager was updated.

--- base/src/main/java/com/resurrection/base/component/BiometricManager.kt
+++ base/src/main/java/com/resurrection/base/component/BiometricManager.kt
@@ -10,47 +10,51 @@ import androidx.core.content.ContextCompat
 import java.util.concurrent.Executor
 
 class BiometricManager {
-    private lateinit var activity:AppCompatActivity
-    private lateinit var executor: Executor 
+
+    private lateinit var activity: AppCompatActivity
+    private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
-    fun init(activity: AppCompatActivity) { this.activity = activity }
-
-     fun setup(
-         title: String,
-         subtitle: String,
-         description: String,
-         biometricIsAvailable:()->Unit,
-         biometricIsUnAvailable:()->Unit,
-         success:(BiometricPrompt.AuthenticationResult)->Unit,
-         error:()->Unit)
-     {
-      if (this::activity.isInitialized){
-          executor = ContextCompat.getMainExecutor(activity)
-
-          if (isBiometricHardWareAvailable(activity)) {
-              initBiometricPrompt(
-                  title,subtitle
-                  ,description
-              )
-              biometricIsAvailable()
-          } else {
-              if (deviceHasPasswordPinLock(activity)) {
-                  initBiometricPrompt(
-                      title,subtitle
-                      ,description)
-                  biometricIsAvailable()
-              } else biometricIsUnAvailable()
-
-          }
-          setBiometricPrompt(activity,success,error)
-      }
+    fun init(activity: AppCompatActivity) {
+        this.activity = activity
     }
 
-    fun authenticate() { if (this::activity.isInitialized) biometricPrompt.authenticate(promptInfo) }
+    fun setup(
+        title: String,
+        subtitle: String,
+        description: String,
+        biometricIsAvailable: () -> Unit,
+        biometricIsUnAvailable: () -> Unit,
+        success: (BiometricPrompt.AuthenticationResult) -> Unit,
+        error: () -> Unit
+    ) {
+        if (this::activity.isInitialized) {
+            executor = ContextCompat.getMainExecutor(activity)
 
-    private fun setBiometricPrompt(activity: AppCompatActivity, success:(BiometricPrompt.AuthenticationResult)->Unit, error:()->Unit) {
+            if (isBiometricHardWareAvailable()) {
+                initBiometricPrompt(title, subtitle, description)
+                biometricIsAvailable()
+            } else {
+                if (deviceHasPasswordPinLock()) {
+                    initBiometricPrompt(title, subtitle, description)
+                    biometricIsAvailable()
+                } else biometricIsUnAvailable()
+
+            }
+            setBiometricPrompt(activity, success, error)
+        }
+    }
+
+    fun authenticate() {
+        if (this::activity.isInitialized) biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun setBiometricPrompt(
+        activity: AppCompatActivity,
+        success: (BiometricPrompt.AuthenticationResult) -> Unit,
+        error: () -> Unit
+    ) {
         biometricPrompt = BiometricPrompt(activity, executor,
             object : BiometricPrompt.AuthenticationCallback() {
 
@@ -77,7 +81,8 @@ class BiometricManager {
         description: String,
     ) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val authFlag = BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG
+            val authFlag =
+                BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG
             promptInfo = BiometricPrompt.PromptInfo.Builder()
                 .setTitle(title)
                 .setSubtitle(subtitle)
@@ -92,44 +97,27 @@ class BiometricManager {
                 .setDeviceCredentialAllowed(true)
                 .build()
         }
-
-
-
     }
 
-    private fun isBiometricHardWareAvailable(con: Context): Boolean {
-        var result = false
-        val biometricManager = BiometricManager.from(con)
+    private fun isBiometricHardWareAvailable(): Boolean {
+        val biometricManager = BiometricManager.from(activity)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            when (biometricManager.canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
-                BiometricManager.BIOMETRIC_SUCCESS -> result = true
-                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
-                BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED ->
-                    result = true
-                BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED ->
-                    result = true
-                BiometricManager.BIOMETRIC_STATUS_UNKNOWN ->
-                    result = false
-            }
-        } else {
-            when (biometricManager.canAuthenticate()) {
-                BiometricManager.BIOMETRIC_SUCCESS -> result = true
-                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result = false
-                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> result = false
-            }
+        val authIntKey =
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) biometricManager.canAuthenticate()
+            else  biometricManager.canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL or BiometricManager.Authenticators.BIOMETRIC_STRONG)
+
+        return when (authIntKey) {
+            BiometricManager.BIOMETRIC_SUCCESS -> true
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> false
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> false
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> false
+            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> true
+            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> true
+            BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> false
+            else -> false
         }
-        return result
     }
 
-
-    private fun deviceHasPasswordPinLock(con: Context): Boolean {
-        val keymgr = con.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
-        if (keymgr.isKeyguardSecure)
-            return true
-        return false
-    }
+    private fun deviceHasPasswordPinLock() =
+        (activity.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager).isKeyguardSecure
 }
