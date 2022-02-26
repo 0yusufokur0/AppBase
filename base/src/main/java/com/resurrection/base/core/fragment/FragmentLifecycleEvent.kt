e7cd928a6c7d07bc5a5c885dfe84cf2aceabc1ef
Yusuf Okur <yusuf.okur@veripark.com>
2022-02-26 20:09:37 +0300

    FragmentLifecycleEvent was added.

---
+++ base/src/main/java/com/resurrection/base/core/fragment/FragmentLifecycleEvent.kt
@@ -0,0 +1,13 @@
+package com.resurrection.base.core.fragment
+
+enum class FragmentLifecycleEvent {
+    ON_CREATE,
+    ON_CREATE_VIEW,
+    ON_VIEW_CREATED,
+    ON_START,
+    ON_RESUME,
+    ON_PAUSE,
+    ON_STOP,
+    ON_DESTROY_VIEW,
+    ON_DESTROY,
+}
