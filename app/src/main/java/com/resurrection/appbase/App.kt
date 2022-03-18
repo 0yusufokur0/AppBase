65a1dd6721b08524403ebdf24a567f6f913e127d
yusuf.okur <yusuf.okur@veripark.com>
2022-03-18 22:56:22 +0300

    Annotation was removed.

--- app/src/main/java/com/resurrection/appbase/App.kt
+++ app/src/main/java/com/resurrection/appbase/App.kt
@@ -1,13 +1,7 @@
 package com.resurrection.appbase
 
-import com.example.Main
-import com.example.processor.AppConstant
 import com.resurrection.base.core.application.BaseApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : BaseApplication()
-@AppConstant("myValue","myProp")
-fun main() {
-    println(Main.myValue)
-}
