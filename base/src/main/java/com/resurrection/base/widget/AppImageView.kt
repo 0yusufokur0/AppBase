81741811bf5922a3f3d3c0a622aa2ee4d22d55d3
yusuf.okur <yusuf.okur@veripark.com>
2022-03-20 12:35:01 +0300

    Background was added.

--- base/src/main/java/com/resurrection/base/widget/AppImageView.kt
+++ base/src/main/java/com/resurrection/base/widget/AppImageView.kt
@@ -3,10 +3,15 @@ package com.resurrection.base.widget
 import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.imageview.ShapeableImageView
+import com.resurrection.base.R
 
 
 class AppImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ShapeableImageView(context, attrs, defStyleAttr)
+) : ShapeableImageView(context, attrs, defStyleAttr){
+    init {
+        setBackgroundResource(R.drawable.rounded_view_bg)
+    }
+}
