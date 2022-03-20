ee8845d1b04e5f1fae73e7930488d819a4e89b6c
yusuf.okur <yusuf.okur@veripark.com>
2022-03-20 12:31:19 +0300

    AppConstraintLayout was added.

--- app/src/main/java/com/resurrection/appbase/ui/photo/PhotosFragment.kt
+++ app/src/main/java/com/resurrection/appbase/ui/photo/PhotosFragment.kt
@@ -1,54 +1,24 @@
 package com.resurrection.appbase.ui.photo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
-import com.resurrection.appbase.BR
+import android.view.MotionEvent
 import com.resurrection.appbase.R
-import com.resurrection.appbase.data.model.photos.PhotoModelItem
 import com.resurrection.appbase.databinding.FragmentPhotosBinding
-import com.resurrection.base.core.adapter.BaseAdapter
 import com.resurrection.base.core.fragment.BaseFragment
-import com.resurrection.base.general.toast
-import com.resurrection.base.widget.setGridLayoutManager
+import com.resurrection.base.util.startCustomAnimation
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
 
 @AndroidEntryPoint
 class PhotosFragment : BaseFragment<FragmentPhotosBinding, PhotosViewModel>
     (R.layout.fragment_photos, PhotosViewModel::class.java) {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun init(savedInstanceState: Bundle?) {
 
-
-        binding.recyclerView.setGridLayoutManager(2)
-
-        val adapter = BaseAdapter(R.layout.photo_item, BR.photoItem, arrayListOf<PhotoModelItem>()){
+        binding.buttonAddPhoto.setOnClickListener {
 
         }
-
-
-        binding.appButton.setOnClickListener {
-            adapter.filterBy("harum"){ it.title }
-            loadingIndicator.show()
-        }
-
-        viewModel.getPhotos()
-        viewModel.photos.observeData(success = {
-            it?.let {
-
-                adapter.addAll(it)
-                binding.recyclerView.adapter = adapter
-
-            }
-        }
-        )
     }
-
 
 }
