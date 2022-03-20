f1210da8a4f6e60ae1cb05d9560cddd15b9d2c28
yusuf.okur <yusuf.okur@veripark.com>
2022-03-20 12:32:47 +0300

    SetClickAnimation method was added.

--- base/src/main/java/com/resurrection/base/util/BindingAdapter.kt
+++ base/src/main/java/com/resurrection/base/util/BindingAdapter.kt
@@ -1,11 +1,15 @@
 package com.resurrection.base.util
 
+import android.annotation.SuppressLint
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import coil.imageLoader
 import coil.loadAny
 import coil.transform.RoundedCornersTransformation
+import com.resurrection.base.R
 
 @BindingAdapter("loadImage")
 fun ImageView.loadImage(image: Any?) {
@@ -19,6 +23,24 @@ fun ImageView.loadImage(image: Any?) {
         loadAny(image, this.context.imageLoader) {
             placeholder(circularProgressDrawable)
             transformations(RoundedCornersTransformation(12f))
+        }
+    }
+}
+
+@SuppressLint("ClickableViewAccessibility")
+@BindingAdapter("setClickAnimation")
+fun View.setClickAnimation(enable:Boolean = false) {
+    if (enable) {
+        setOnTouchListener { v, event ->
+            background.alpha = 200
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> background.alpha = 200
+                MotionEvent.ACTION_UP -> {
+                    startCustomAnimation(R.anim.button_action_down)
+                    background.alpha = 255
+                }
+            }
+            true
         }
     }
 }
