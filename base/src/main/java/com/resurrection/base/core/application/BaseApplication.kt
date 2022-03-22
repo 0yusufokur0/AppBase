a232d0c4d0db5482c63dd49a7637c7176130e347
yusuf.okur <yusuf.okur@veripark.com>
2022-03-22 17:05:02 +0300

    BaseApplication was updated.

--- base/src/main/java/com/resurrection/base/core/application/BaseApplication.kt
+++ base/src/main/java/com/resurrection/base/core/application/BaseApplication.kt
@@ -1,5 +1,33 @@
 package com.resurrection.base.core.application
 
 import androidx.multidex.MultiDexApplication
+import com.resurrection.base.component.*
+import javax.inject.Inject
 
-abstract class BaseApplication : MultiDexApplication()
+abstract class BaseApplication : MultiDexApplication(){
+
+    @Inject
+    lateinit var appState: AppState
+    @Inject
+    lateinit var dataHolder: DataHolderManager
+    @Inject
+    lateinit var sharedPreferences: SharedPreferencesManager
+    @Inject
+    lateinit var loggerManager: LoggerManager
+    @Inject
+    lateinit var loadingIndicator: AppLoadingIndicator
+    @Inject
+    lateinit var networkManager: NetworkManager
+    @Inject
+    lateinit var securityManager: SecurityManager
+    @Inject
+    lateinit var biometricManager: BiometricManager
+
+    abstract fun init()
+
+    override fun onCreate() {
+        super.onCreate()
+        init()
+    }
+
+}
