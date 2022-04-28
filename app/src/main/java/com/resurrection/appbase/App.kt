af729f0bb023125d75878b889396d26edeaec60d
Yusuf Okur <0yusufokur0@gmail.com>
2022-04-28 22:01:28 +0300

    LoggerInterceptor was added.

--- app/src/main/java/com/resurrection/appbase/App.kt
+++ app/src/main/java/com/resurrection/appbase/App.kt
@@ -1,11 +1,23 @@
 package com.resurrection.appbase
 
 import com.resurrection.base.core.application.BaseApplication
+import com.resurrection.base.core.network.LoggerInterceptor
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.OkHttpClient
+import java.util.logging.Logger
 
 @HiltAndroidApp
 class App : BaseApplication() {
     override fun init() {
+        initOkHttpClient()
+    }
 
+    private fun initOkHttpClient(){
+        val okHttpClient =
+            OkHttpClient()
+                .newBuilder()
+                .addInterceptor(LoggerInterceptor(loggerManager))
+                .build()
+        networkManager.init(okHttpClient,BuildConfig.API_URL)
     }
 }
--- base/src/main/java/com/resurrection/base/core/network/LoggerInterceptor.kt
+++ base/src/main/java/com/resurrection/base/core/network/LoggerInterceptor.kt
@@ -1,17 +1,43 @@
 package com.resurrection.base.core.network
 
-import kotlinx.coroutines.DisposableHandle
+import com.resurrection.base.component.LoggerManager
+import com.resurrection.base.util.DateTimeUtils
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
-abstract class LoggerInterceptor :Interceptor {
+open class LoggerInterceptor(private val loggerManager: LoggerManager) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain
             .request()
             .newBuilder()
             .build()
-
         val response = chain.proceed(request)
+        logRequestAndResponse(request, response)
         return response
     }
+
+    private fun logRequestAndResponse(request: Request, response: Response) {
+    val requestTime = "Request Date Time: ${DateTimeUtils.getCurrentDateTime()}"
+        val requestLogString =
+                    "Request url: ${request.url } \n" +
+                    "Request method: ${ request.method } \n" +
+                    "Request headers: ${ request.headers } \n" +
+                    "Request body: ${request.body } \n"
+
+
+        val responseLogString =
+                    "Request url: ${response.request.url } \n" +
+                    "Request headers: ${ response.headers} \n" +
+                    "Request body: ${response.body?.string() } \n"
+                    "Request code: ${ response.code} \n"
+                    "Request message  ${ response.message } \n"
+
+        loggerManager.d("-------------------------------------------------------")
+        loggerManager.d(requestTime)
+        loggerManager.d(requestLogString)
+        loggerManager.d(responseLogString)
+        loggerManager.d("-------------------------------------------------------")
+    }
+
 }
