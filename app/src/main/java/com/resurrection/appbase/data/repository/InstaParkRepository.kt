9dddad3b7bf0df91912b05675a6cf044ff335929
yusuf.okur <yusuf.okur@veripark.com>
2022-05-02 09:27:59 +0300

    InstaParkRepository was updated.

--- app/src/main/java/com/resurrection/appbase/data/repository/InstaParkRepository.kt
+++ app/src/main/java/com/resurrection/appbase/data/repository/InstaParkRepository.kt
@@ -25,55 +25,16 @@ class InstaParkRepository @Inject constructor(
     val instaParkApiService: InstaParkApiService,
     val networkManager: NetworkManager) {
 
-    private var okHttpClient = OkHttpClient().newBuilder().build()
 
     val medicineListType: Type = object : TypeToken<ArrayList<PhotoModelItem>>() {}.type
 
     suspend fun getUsers() = getData { instaParkApiService.getUsers() }
     suspend fun getUser(id: String) = getData { instaParkApiService.getUser(id) }
     suspend fun getPosts() = getData { instaParkApiService.getPosts() }
-    suspend fun getPhotos() = test(
-            path = "https://jsonplaceholder.typicode.com/photos",
+    suspend fun getPhotos() = networkManager.newRequest(
+            path = "/photos",
             responseType = PhotoModel::class.java,
         )
 
 
-    fun <T> test(
-        okHttpClient: OkHttpClient = this.okHttpClient,
-        baseUrl: String = "",
-        method: String = NetworkManager.GET,
-        body: String? = null,
-        headers: Map<String, String>? = null,
-        path: String,
-        responseType: Class<T>,
-        map:(responseBody: String) -> T =  {
-            Gson().fromJson(it,responseType)
-        }
-    ) = flow {
-        val requestBuilder = Request.Builder()
-            .url(baseUrl + path)
-            .method(method, body?.toRequestBody())
-
-        headers?.let { requestBuilder.headers(headers.toHeaders()) }
-
-        val request = requestBuilder.build()
-
-        var resultResource: Resource<T>? = null
-        try {
-            val response = okHttpClient.newCall(request).execute()
-            if (response.isSuccessful) {
-                response.body?.apply {
-                    val mapResult = map(this.string())
-                    resultResource = if (map != null) Resource.Success(mapResult)
-                    else Resource.Error(Exception("map is null"))
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            resultResource = Resource.Error(e)
-        }
-        if (resultResource == null) resultResource = Resource.Loading()
-
-        emit(resultResource!!)
-    }.flowOn(Dispatchers.IO)
 }
