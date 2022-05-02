0ee78d3adc95974b6892b28812b0758a0a82e48f
yusuf.okur <yusuf.okur@veripark.com>
2022-05-02 12:09:19 +0300

    LoggerInterceptor was updated.

--- base/src/main/java/com/resurrection/base/core/network/LoggerInterceptor.kt
+++ base/src/main/java/com/resurrection/base/core/network/LoggerInterceptor.kt
@@ -2,45 +2,70 @@ package com.resurrection.base.core.network
 
 import com.resurrection.base.component.LoggerManager
 import com.resurrection.base.util.DateTimeUtils
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+import java.nio.charset.StandardCharsets
+
 
 open class LoggerInterceptor(private val loggerManager: LoggerManager) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain
-            .request()
+        val request = chain.request().newBuilder().build()
+        val tempResponse = chain.proceed(request)
+        val tempBody = tempResponse.body
+
+        val resultResponse = tempResponse
             .newBuilder()
+            .body(tempBody?.string()?.toResponseBody(tempBody.contentType()))
             .build()
-        val response = chain.proceed(request)
-        logRequestAndResponse(request, response)
-        return response
+
+        logRequestAndResponse(request, resultResponse)
+
+        return resultResponse
+
     }
 
     private fun logRequestAndResponse(request: Request, response: Response) {
-    val requestTime = "Request Date Time: ${DateTimeUtils.getCurrentDateTime()}"
+
+        val requestTime = "\t Request Date Time: ${DateTimeUtils.getCurrentDateTime()}"
+        val requestUrl = request.url.toString()
+        val requestMethod = request.method
+        val requestHeaders = request.headers.toString()
+        val requestBody = request.body?.toString() ?: ""
+
+        val responseHeaders = response.headers.toString()
+        val responseBody = response.peekBody(Long.MAX_VALUE).string()
+        val responseCode = response.code
+        val responseMessage = response.message
+        val mresponseBody = response.body?.source()?.buffer?.clone()?.readString(StandardCharsets.UTF_8)
+
         val requestLogString =
-                    "Request url: ${request.url } \n" +
-                    "Request method: ${ request.method } \n" +
-                    "Request headers: ${ request.headers } \n" +
-                    "Request body: ${request.body } \n"
-
-
+                    "\n" +
+                    "\t URL: $requestUrl \n" +
+                    "\t Method: $requestMethod \n" +
+                    "\t Headers: $requestHeaders \n" +
+                    "\t Body: $requestBody \n"
         val responseLogString =
-                    "Request headers: ${ response.headers} \n" +
-                    "Request body: ${response.body?.string() } \n"
-                    "Request code: ${ response.code} \n"
-                    "Request message  ${ response.message } \n"
+                    "\n" +
+                    "\t Headers: $responseHeaders \n" +
+                    "\t Body: $mresponseBody \n" +
+                    "\t Message: $responseMessage \n"+
+                    "\t Code: $responseCode \n"
+        val resultLogString =
+                " .\n"+
+                "┌────── Request ────────────────────────────────────────────────────────────────────────" +
+                "\n" +
+                requestTime +
+                requestLogString +
+                "└───────────────────────────────────────────────────────────────────────────────────────" +
+                "\n" +
+                "┌────── Response ───────────────────────────────────────────────────────────────────────" +
+                responseLogString +
+                "└───────────────────────────────────────────────────────────────────────────────────────"
 
-        loggerManager.d("-------------------------------------------------------")
-        loggerManager.d(requestTime)
-        loggerManager.d(requestLogString)
-        loggerManager.d(responseLogString)
-        loggerManager.d("-------------------------------------------------------")
+        loggerManager.d(resultLogString)
+
     }
 
 }
