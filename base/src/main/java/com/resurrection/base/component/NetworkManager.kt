c0e52f9445371c00bf4d3331db6117ae9b78a2db
yusuf.okur <yusuf.okur@veripark.com>
2022-05-02 09:33:06 +0300

    OkHttp was removed.

--- base/src/main/java/com/resurrection/base/component/NetworkManager.kt
+++ base/src/main/java/com/resurrection/base/component/NetworkManager.kt
@@ -21,62 +21,11 @@ class NetworkManager @Inject constructor(
     @ApplicationContext val context: Context,
     val typeConverter: TypeConverter
 ) {
-    private var _okHttpClient = OkHttpClient().newBuilder().build()
-    val okHttpClient = _okHttpClient
-    private var baseUrl = ""
+
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     private val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
     private var _isNetworkAvailable = checkNetworkAvailable()
     val isNetworkAvailable: Boolean = _isNetworkAvailable
-
-    companion object {
-        const val GET = "GET"
-        const val POST = "POST"
-        const val PUT = "PUT"
-        const val DELETE = "DELETE"
-    }
-
-    fun init(okHttpClient: OkHttpClient = _okHttpClient,baseUrl: String) {
-        this.baseUrl = baseUrl
-        this._okHttpClient = okHttpClient
-    }
-
-    fun <T> newRequest(
-        okHttpClient: OkHttpClient = this.okHttpClient,
-        baseUrl: String = this.baseUrl,
-        method: String = GET,
-        body: String? = null,
-        headers: Map<String, String>? = null,
-        path: String,
-        responseType: Class<T>,
-        map:(responseBody: String) -> T =  { typeConverter.fromJson(it, responseType) }
-    ) = flow {
-        val requestBuilder = Request.Builder()
-            .url(baseUrl + path)
-            .method(method, body?.toRequestBody())
-
-        headers?.let { requestBuilder.headers(headers.toHeaders()) }
-
-        val request = requestBuilder.build()
-
-        var resultResource: Resource<T>? = null
-        try {
-            val response = okHttpClient.newCall(request).execute()
-            if (response.isSuccessful) {
-                response.body?.apply {
-                    val mapResult = map(this.string())
-                    resultResource = if (map != null) Resource.Success(mapResult)
-                    else Resource.Error(Exception("map is null"))
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            resultResource = Resource.Error(e)
-        }
-        if (resultResource == null) resultResource = Resource.Loading()
-
-        emit(resultResource!!)
-    }.flowOn(Dispatchers.IO)
 
     fun checkNetworkAvailable(): Boolean =
         try {
