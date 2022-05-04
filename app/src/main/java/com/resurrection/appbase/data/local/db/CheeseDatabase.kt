c725fa0c78f98073d31df3f1df067fc41ad47e81
yusuf.okur <yusuf.okur@veripark.com>
2022-05-04 13:32:01 +0300

    package hierarchy was updated.

--- app/src/main/java/com/resurrection/appbase/data/local/db/CheeseDatabase.kt
+++ app/src/main/java/com/resurrection/appbase/data/local/db/CheeseDatabase.kt
@@ -1,10 +1,11 @@
-package com.resurrection.appbase.data.local.dao
+package com.resurrection.appbase.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.resurrection.appbase.data.local.dao.CheeseDao
 import com.resurrection.appbase.data.model.cheese.Cheese
 
 @Database(entities = [Cheese::class], version = 1)
 abstract class CheeseDatabase : RoomDatabase() {
-    abstract fun cheeseDao():CheeseDao
+    abstract fun cheeseDao(): CheeseDao
 }
--- app/src/main/java/com/resurrection/appbase/di/CheeseDaoModule.kt
+++ app/src/main/java/com/resurrection/appbase/di/CheeseDaoModule.kt
@@ -2,10 +2,8 @@ package com.resurrection.appbase.di
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.resurrection.appbase.data.local.dao.CheeseDao
-import com.resurrection.appbase.data.local.dao.CheeseDatabase
+import com.resurrection.appbase.data.local.db.CheeseDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,8 +17,8 @@ object CheeseDaoModule {
 
     @Provides
     @Singleton
-    fun provideCheeseDatabase(@ApplicationContext context: Context):CheeseDatabase =
-        Room.databaseBuilder(context,CheeseDatabase::class.java,"database_name").build()
+    fun provideCheeseDatabase(@ApplicationContext context: Context): CheeseDatabase =
+        Room.databaseBuilder(context, CheeseDatabase::class.java,"database_name").build()
 
     @Provides
     @Singleton
