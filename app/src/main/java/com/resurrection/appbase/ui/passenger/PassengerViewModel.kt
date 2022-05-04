7b9823ef6522e942a3d9f2bb60e3ee47bdc5cd4f
yusuf.okur <yusuf.okur@veripark.com>
2022-05-04 22:48:17 +0300

    PagingSource was added.

--- app/src/main/java/com/resurrection/appbase/ui/passenger/PassengerViewModel.kt
+++ app/src/main/java/com/resurrection/appbase/ui/passenger/PassengerViewModel.kt
@@ -6,13 +6,22 @@ import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import com.resurrection.appbase.data.remote.PassengersApiService
 import com.resurrection.base.core.viewmodel.BaseViewModel
+import com.resurrection.base.core.viewmodel.pagingDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PassengerViewModel @Inject constructor(val passengersApiService: PassengersApiService): BaseViewModel() {
-    val passengers =
-        Pager(config = PagingConfig(pageSize = 10, prefetchDistance = 2), pagingSourceFactory = {
-            PassengersDataSource(passengersApiService)
-        }).flow.cachedIn(viewModelScope)
+class PassengerViewModel @Inject constructor(val passengersApiService: PassengersApiService) :
+    BaseViewModel() {
+
+    val passengers = Pager(config = PagingConfig(pageSize = 10, prefetchDistance = 2),
+        pagingSourceFactory = {
+            pagingDataSource(
+                request = { passengersApiService.getPassengersData(it) },
+                data = { it.data },
+                totalPages = { it.totalPages }
+            )
+        }
+    ).flow.cachedIn(viewModelScope)
+
 }
