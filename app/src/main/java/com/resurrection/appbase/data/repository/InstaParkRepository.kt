e25deff72e6648196f25c16e04fe1d520d8fe1d5
Yusuf Okur <yusuf.okur@veripark.com>
2022-06-18 16:13:01 +0300

    Unused components was removed.

--- app/src/main/java/com/resurrection/appbase/data/repository/InstaParkRepository.kt
+++ app/src/main/java/com/resurrection/appbase/data/repository/InstaParkRepository.kt
@@ -1,31 +1,27 @@
 package com.resurrection.appbase.data.repository
 
-import com.google.gson.reflect.TypeToken
 import com.resurrection.appbase.data.model.photos.PhotoModel
-import com.resurrection.appbase.data.model.photos.PhotoModelItem
 import com.resurrection.appbase.data.remote.InstaParkApiService
 import com.resurrection.base.components.network.OkHttpClientManager
 import com.resurrection.base.network.getData
-import java.lang.reflect.Type
 import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 
 class InstaParkRepository @Inject constructor(
-    val instaParkApiService: InstaParkApiService,
-    val okHttpClientManager: OkHttpClientManager
-    ) {
-
-
-    val medicineListType: Type = object : TypeToken<ArrayList<PhotoModelItem>>() {}.type
+    private val instaParkApiService: InstaParkApiService,
+    private val okHttpClientManager: OkHttpClientManager
+) {
 
     suspend fun getUsers() = getData { instaParkApiService.getUsers() }
+
     suspend fun getUser(id: String) = getData { instaParkApiService.getUser(id) }
+
     suspend fun getPosts() = getData { instaParkApiService.getPosts() }
-    suspend fun getPhotos() = okHttpClientManager.newRequest(
-            path = "/photos",
-            responseType = PhotoModel::class.java,
-        )
+
+    fun getPhotos() = okHttpClientManager.newRequest(
+        path = "/photos",
+        responseType = PhotoModel::class.java,
+    )
 
 
 }
