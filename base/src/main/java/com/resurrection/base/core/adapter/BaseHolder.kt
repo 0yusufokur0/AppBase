463964bb235741f03ac99cc941d12feb2177fcca
Yusuf Okur <yusuf.okur@veripark.com>
2022-06-18 15:16:37 +0300

    Variables was renamed.

--- base/src/main/java/com/resurrection/base/core/adapter/BaseHolder.kt
+++ base/src/main/java/com/resurrection/base/core/adapter/BaseHolder.kt
@@ -3,13 +3,15 @@ package com.resurrection.base.core.adapter
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
-open class BaseHolder<T>(
-    open val binding: ViewDataBinding,
+open class BaseHolder<Model,VDB:ViewDataBinding>(
+    open val binding: VDB,
     open val itemId: Int?,
-    open val onItemClick: ((T) -> Unit)? = null
+    open val onItemClick: ((Model) -> Unit)? = null,
+    open val setBind :((VDB,Model) -> Unit)? = null
 ) : RecyclerView.ViewHolder(binding.root) {
-    open fun bind(item: T) {
+    open fun bind(item: Model) {
         itemId?.let { binding.setVariable(it, item) }
+        setBind?.invoke(binding,item)
         itemView.setOnClickListener { onItemClick?.invoke((item)) }
     }
 }
