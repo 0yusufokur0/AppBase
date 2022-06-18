1bcc8761b33188e00c182735511cfe2a14816431
Yusuf Okur <yusuf.okur@veripark.com>
2022-06-18 15:44:50 +0300

    Widget was updated.

--- base/src/main/java/com/resurrection/base/extensions/Widget.kt
+++ base/src/main/java/com/resurrection/base/extensions/Widget.kt
@@ -6,16 +6,31 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.resurrection.base.core.adapter.BaseAdapter
 
-fun RecyclerView.setGridLayoutManager(spanCount:Int) =
+fun RecyclerView.setGridLayoutManager(spanCount: Int) =
     GridLayoutManager(this.context, spanCount).also { this.layoutManager = it }
 
-fun RecyclerView.setVerticalLinearLayoutManager(reverseLayout:Boolean = false) =
-    LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, reverseLayout).also { this.layoutManager = it }
+fun RecyclerView.setVerticalLinearLayoutManager(reverseLayout: Boolean = false) =
+    LinearLayoutManager(
+        this.context,
+        LinearLayoutManager.VERTICAL,
+        reverseLayout
+    ).also { this.layoutManager = it }
 
-fun RecyclerView.setHorizontalLinearLayoutManager(reverseLayout:Boolean = false) =
-    LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, reverseLayout).also { this.layoutManager = it }
+fun RecyclerView.setHorizontalLinearLayoutManager(reverseLayout: Boolean = false) =
+    LinearLayoutManager(
+        this.context,
+        LinearLayoutManager.HORIZONTAL,
+        reverseLayout
+    ).also { this.layoutManager = it }
 
-fun <Model,VDB:ViewDataBinding> RecyclerView.init(adapter: BaseAdapter<Model,VDB>,layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)){
+fun <Model : Any, VDB : ViewDataBinding> RecyclerView.init(
+    adapter: BaseAdapter<Model, VDB>,
+    layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(
+        this.context,
+        LinearLayoutManager.VERTICAL,
+        false
+    )
+) {
     this.layoutManager = layoutManager
     this.adapter = adapter
 }
