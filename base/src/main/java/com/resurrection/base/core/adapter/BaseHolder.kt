70d13c7a9c7cb6e17105221b96f974a57a206e8c
Yusuf Okur <yusuf.okur@veripark.com>
2022-06-18 15:46:02 +0300

    CoreAdapter onClick and onLongClick was added.

--- base/src/main/java/com/resurrection/base/core/adapter/BaseHolder.kt
+++ base/src/main/java/com/resurrection/base/core/adapter/BaseHolder.kt
@@ -6,12 +6,17 @@ import androidx.recyclerview.widget.RecyclerView
 open class BaseHolder<Model,VDB:ViewDataBinding>(
     open val binding: VDB,
     open val itemId: Int?,
-    open val onItemClick: ((Model) -> Unit)? = null,
+    private val itemClick: ((Model) -> Unit)? = null,
+    private val itemLongClick: ((Model) -> Unit)? = null,
     open val setBind :((VDB,Model) -> Unit)? = null
 ) : RecyclerView.ViewHolder(binding.root) {
     open fun bind(item: Model) {
         itemId?.let { binding.setVariable(it, item) }
         setBind?.invoke(binding,item)
-        itemView.setOnClickListener { onItemClick?.invoke((item)) }
+        itemView.setOnClickListener { itemClick?.invoke((item)) }
+        itemView.setOnLongClickListener {
+            itemLongClick?.invoke((item))
+            true
+        }
     }
 }
