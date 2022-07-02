6d2595fbbeefce4652a1fe288672e561df1dcdf3
Yusuf Okur <yusuf.okur@veripark.com>
2022-07-02 13:55:24 +0300

    AppState was updated.

--- base/src/main/java/com/resurrection/base/components/appstate/AppState.kt
+++ base/src/main/java/com/resurrection/base/components/appstate/AppState.kt
@@ -1,10 +1,29 @@
 package com.resurrection.base.components.appstate
 
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import com.resurrection.base.components.dataholder.DataHolderManager
+import com.resurrection.base.components.network.NetworkManager
+import com.resurrection.base.components.security.SecurityManager
+import com.resurrection.base.utils.BaseConstants
 import javax.inject.Inject
 
-data class AppState @Inject constructor(
-    var isAppForeground: Boolean,
-    var isAppLightMode: Boolean,
-    var isNetworkAvailable: Boolean,
-    var isRooted: Boolean
-)
+class AppState @Inject constructor(
+    private val context: Context,
+    private val dataHolderManager: DataHolderManager,
+    private val networkManager: NetworkManager,
+    private val securityManager: SecurityManager
+) {
+    val isAppForeground: Boolean
+        get() = dataHolderManager.getBoolean(BaseConstants.IS_FOREGROUND, false)
+
+    val isAppLightMode: Boolean
+        get() = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES
+
+    val isNetworkAvailable: Boolean
+        get() = networkManager.checkNetworkAvailable()
+
+    val isRooted: Boolean
+        get() = securityManager.isRooted()
+}
