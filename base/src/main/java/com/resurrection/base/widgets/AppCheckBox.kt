2b77f33006ba7ab0908e46e1894e52c249a536a1
yusuf.okur <yusuf.okur@veripark.com>
2022-09-09 23:33:18 +0300

    AppCheckBox was added.

---
+++ base/src/main/java/com/resurrection/base/widgets/AppCheckBox.kt
@@ -0,0 +1,10 @@
+package com.resurrection.base.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.resurrection.base.R
+
+class AppCheckBox @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.checkboxStyle
+) : MaterialCheckBox(context, attrs, defStyleAttr)
