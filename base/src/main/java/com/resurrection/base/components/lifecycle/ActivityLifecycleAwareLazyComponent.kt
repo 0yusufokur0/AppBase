fe0047629946b4fe9c0b7796c93c0b41fdc2dbd7
yusuf.okur <yusuf.okur@veripark.com>
2022-09-26 22:58:16 +0300

    Lifecycle aware components was added.

---
+++ base/src/main/java/com/resurrection/base/components/lifecycle/ActivityLifecycleAwareLazyComponent.kt
@@ -0,0 +1,33 @@
+package com.resurrection.base.components.lifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import com.resurrection.base.core.activity.LifecycleActivity
+
+class ActivityLifecycleAwareLazyComponent<T>(
+    private val lifecycleActivity: LifecycleActivity,
+    private val instanceCreator: () -> T,
+    private val observer: LifecycleEventObserver? = null
+) : Lazy<T>, LifecycleEventObserver {
+
+    private var cached: T? = null
+
+    override val value: T
+        get() {
+            cached ?: run {
+                cached = instanceCreator.invoke()
+                lifecycleActivity.lifecycle.addObserver(this)
+            }
+            return cached!!
+        }
+
+    override fun isInitialized(): Boolean = cached != null
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        observer?.onStateChanged(source, event)
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            lifecycleActivity.lifecycle.removeObserver(this)
+        }
+    }
+}
---
+++ base/src/main/java/com/resurrection/base/components/lifecycle/FragmentLifecycleAwareLazyComponent.kt
@@ -0,0 +1,33 @@
+package com.resurrection.base.components.lifecycle
+
+import androidx.lifecycle.LifecycleOwner
+import com.resurrection.base.core.fragment.LifecycleFragment
+import com.resurrection.base.utils.fragment.FragmentLifecycleEvent
+import com.resurrection.base.utils.fragment.FragmentLifecycleEventObserver
+
+class FragmentLifecycleAwareLazyComponent<T>(
+    private val lifecycleFragment: LifecycleFragment,
+    private val instanceCreator: () -> T,
+    private val observer: FragmentLifecycleEventObserver
+) : Lazy<T>, FragmentLifecycleEventObserver {
+
+    private var cached: T? = null
+
+    override val value: T
+        get() {
+            cached ?: run {
+                cached = instanceCreator.invoke()
+                lifecycleFragment.addLifecycleObserver(this)
+            }
+            return cached!!
+        }
+
+    override fun isInitialized(): Boolean = cached != null
+
+    override fun onStateChanged(owner: LifecycleOwner, event: FragmentLifecycleEvent) {
+        observer.onStateChanged(owner, event)
+        if (event == FragmentLifecycleEvent.ON_DESTROY_VIEW) {
+            lifecycleFragment.removeLifecycleObserver(this)
+        }
+    }
+}
