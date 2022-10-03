a083bd1e5b486d0e1a150a961d65139692f5645e
yusuf.okur <yusuf.okur@veripark.com>
2022-10-03 23:08:44 +0300

    ViewDataBinding delegated extensions was separated.

---
+++ base/src/main/java/com/resurrection/base/extensions/delegated/ViewBinding.kt
@@ -0,0 +1,28 @@
+package com.resurrection.base.extensions.delegated
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewbinding.ViewBinding
+import com.resurrection.base.components.lifecycle.activity.ActivityLifecycleAwareLazyComponent
+import com.resurrection.base.components.lifecycle.fragment.FragmentLifecycleAwareLazyComponent
+import com.resurrection.base.core.activity.LifecycleActivity
+import com.resurrection.base.core.fragment.LifecycleFragment
+
+
+fun <VB : ViewBinding> LifecycleActivity.viewBinding(
+    initializer: (LayoutInflater) -> VB
+) = ActivityLifecycleAwareLazyComponent(
+    lifecycleActivity = this,
+    instanceCreator = { initializer.invoke(layoutInflater) }
+)
+
+
+fun <VB : ViewBinding> LifecycleFragment.viewBinding(
+    initializer: (LayoutInflater) -> VB
+) = FragmentLifecycleAwareLazyComponent(
+    lifecycleFragment = this,
+    instanceCreator = { initializer.invoke(layoutInflater) }
+)
+
+fun <VB : ViewBinding> View.viewBinding(initializer: (LayoutInflater) -> VB): Lazy<VB> =
+    lazy { initializer.invoke(LayoutInflater.from(context)) }
