a3f75f66f3a007eee38f91567e4187ebfe5726f4
yusuf.okur <yusuf.okur@veripark.com>
2022-10-13 22:05:51 +0300

    Component creator was added.

--- base/src/main/java/com/resurrection/base/extensions/delegated/navigation/Navigation.kt
+++ base/src/main/java/com/resurrection/base/extensions/delegated/navigation/Navigation.kt
@@ -5,13 +5,10 @@ import androidx.annotation.IdRes
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.resurrection.base.components.lifecycle.activity.ActivityLifecycleAwareLazyComponent
+import com.resurrection.base.components.lifecycle.util.activityComponent
 import com.resurrection.base.core.activity.LifecycleActivity
 
-fun LifecycleActivity.navController(@IdRes id: Int) = ActivityLifecycleAwareLazyComponent(
-    lifecycleActivity = this,
-    instanceCreator = { (supportFragmentManager.findFragmentById(id) as NavHostFragment).navController }
-)
+fun LifecycleActivity.navController(@IdRes id: Int) = activityComponent { (supportFragmentManager.findFragmentById(id) as NavHostFragment).navController }
 
 fun Activity.navController(@IdRes id: Int): Lazy<NavController> =
     lazy { findNavController(id) }
