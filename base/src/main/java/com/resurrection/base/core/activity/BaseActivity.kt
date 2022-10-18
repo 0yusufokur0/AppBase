0687937899d6b4dd12726258d6758c74ceb37148
yusuf.okur <yusuf.okur@veripark.com>
2022-10-18 21:07:54 +0300

    DataBinding params was added.

--- base/src/main/java/com/resurrection/base/core/activity/BaseActivity.kt
+++ base/src/main/java/com/resurrection/base/core/activity/BaseActivity.kt
@@ -7,12 +7,12 @@ import com.resurrection.base.core.viewmodel.BaseViewModel
 import com.resurrection.base.extensions.delegated.viewdatabinding.dataBinding
 import com.resurrection.base.extensions.delegated.viewmodel.viewModel
 
-abstract class BaseActivity<VDB : ViewDataBinding, VM : BaseViewModel>(
+abstract class BaseActivity<VDB:ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes layoutRes: Int,
     viewModelClass: Class<VM>
 ) : LifecycleActivity(layoutRes) {
 
-    protected val binding by dataBinding<VDB>()
+    protected val binding by dataBinding<VDB>(layoutRes)
     protected val viewModel by viewModel(viewModelClass)
 
     abstract override fun init(savedInstanceState: Bundle?)
